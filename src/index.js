d437974bd6fb18a382928bebfd865c0c38dd1baf
Matthew Little <zone117x@gmail.com>
2019-04-05 17:09:57 -0400

    Misc changes for testing and demo purposes

--- src/index.js
+++ src/index.js
@@ -1,33 +1,123 @@
-// import blockstack from 'blockstack'
+import blockstack from 'blockstack'
 
-import * as blockstack from 'blockstack/dist/blockstack'
+import { redirectToSignIn } from 'blockstack/lib/auth/authApp'
 
-document.addEventListener("DOMContentLoaded", function (event) {
-    document.getElementById('signin-button').addEventListener('click', function (event) {
-      event.preventDefault()
-      blockstack.redirectToSignIn(`${window.location.origin}/auth.html`, undefined, ['store_write', 'email'])
-    })
-    document.getElementById('signout-button').addEventListener('click', function (event) {
-      event.preventDefault()
-      blockstack.signUserOut(window.location.href)
-    })
+window.blockstack = blockstack
 
-    function showProfile(profile) {
-      var person = new blockstack.Person(profile)
-      document.getElementById('heading-name').innerHTML = person.name() ? person.name() : "Nameless Person"
-      if (person.avatarUrl()) {
-        document.getElementById('avatar-image').setAttribute('src', person.avatarUrl())
-      }
-      document.getElementById('section-1').style.display = 'none'
-      document.getElementById('section-2').style.display = 'block'
+document.addEventListener('DOMContentLoaded', function () {
+
+  document.getElementById('signin-button').addEventListener('click', function (event) {
+    event.preventDefault()
+    blockstack.redirectToSignIn(
+      window.origin,
+      window.origin + '/manifest.json',
+      ['store_write', 'publish_data']
+    )
+  })
+
+  document.getElementById('signout-button').addEventListener('click', function (event) {
+    event.preventDefault()
+    blockstack.signUserOut(window.origin)
+  })
+
+  function showProfile(profile) {
+    var person = new blockstack.Person(profile)
+    document.getElementById('heading-name').innerHTML = person.name() ? person.name() : "Nameless Person"
+    if (person.avatarUrl()) {
+      document.getElementById('avatar-image').setAttribute('src', person.avatarUrl())
     }
+    document.getElementById('section-1').style.display = 'none'
+    document.getElementById('section-2').style.display = 'block'
+    showStatus()
+  }
 
-    if (blockstack.isUserSignedIn()) {
-      var profile = blockstack.loadUserData().profile
-      showProfile(profile)
-    } else if (blockstack.isSignInPending()) {
-      blockstack.handlePendingSignIn().then(function (userData) {
-        window.location = window.location.origin
+  if (blockstack.isSignInPending()) {
+    blockstack.handlePendingSignIn().then(function () {
+      window.location = window.origin
+    }).catch((error) => {
+      window.alert(`Unexpected error in handlePendingSignIn! ${error}`)
+    })
+  } else if (blockstack.isUserSignedIn()) { // User is already signed in. 
+    var profile = blockstack.loadUserData().profile
+    showProfile(profile)
+  }
+
+  function showStatus() {
+    blockstack.getFile('status.txt').then((statusMsg) => {
+      if (statusMsg) {
+        console.log('Got status message: ' + statusMsg)
+      } else {
+        console.log('Status file does not exist!')
+      }
+      document.getElementById('currentStatus').innerText = statusMsg
+    }).catch((error) => {
+      console.log('Error reading file: ' + error)
+      document.getElementById('currentStatus').innerText = 'Error reading status ' + error
+    })
+  }
+
+  function getStatusForUser(username) {
+    var options = {
+      username: username,
+      decrypt: false
+    }
+    blockstack.getFile('status.txt', options).then((msg) => {
+      console.log(`Got status for ${options.username}: ${msg}`)
+    })
+  }
+
+  document.getElementById('updateStatus').onclick = () => {
+    var statusMsg = document.getElementById('status').value
+    var options = { encrypt: false }
+    blockstack.putFile('status.txt', statusMsg, options).then(() => {
+      console.log('uploaded publicly accessible file!')
+      showStatus()
+    })
+  }
+
+  document.getElementById('photoUpload').onchange = (event) => {
+    if (!event.target.files) return
+    const file = event.target.files[0]
+    const reader = new FileReader()
+    reader.onloadend = () => {
+      const buffer = new Uint8Array(reader.result)
+      blockstack.putFile(file.name, buffer, {
+        contentType: file.type,
+        encrypt: false,
+      }).then((url) => {
+        console.log(`Uploaded: ${url}`)
       })
     }
-  })
+    reader.readAsDataURL(file)
+  }
+
+
+  // testHmac('the shared secret key here', 'the message to hash here')
+  async function testHmac(key, message) {
+
+    const getUtf8Bytes = str =>
+      new Uint8Array(
+        [...unescape(encodeURIComponent(str))].map(c => c.charCodeAt())
+      );
+
+    const keyBytes = getUtf8Bytes(key);
+    const cryptoKey = await crypto.subtle.importKey(
+      'raw', keyBytes, { name: 'HMAC', hash: 'SHA-256' },
+      true, ['sign']
+    );
+
+    const messageBytes = getUtf8Bytes(message);
+    const sig = await crypto.subtle.sign('HMAC', cryptoKey, messageBytes);
+
+    // to lowercase hexits
+    const hex = [...new Uint8Array(sig)].map(b => b.toString(16).padStart(2, '0')).join('');
+    console.log(`HEX: ${hex}`)
+
+    // to base64
+    const b64 = btoa(String.fromCharCode(...new Uint8Array(sig)));
+    console.log(`B64: ${b64}`)
+    return b64;
+  }
+
+
+})
--- webpack.config.js
+++ webpack.config.js
@@ -5,21 +5,11 @@ module.exports = {
     contentBase: path.join(__dirname, 'dist'),
     port: 9000,
     open: true,
-    headers: {
-      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE",
-      "Access-Control-Allow-Headers": "Content-Type",
-      "Access-Control-Allow-Origin": "*"
-    }
+    headers: { 'Access-Control-Allow-Origin': '*' }
   },
-  entry: {
-    index: './src/index.js',
-    auth: './src/auth.js'
-  },
+  entry: { index: './src/index.js' },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
-  },
-  optimization: {
-    usedExports: true
   }
 };
